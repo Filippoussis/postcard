8af84be0a464caa91c99f53afcb26867be13ec24
Andrey Filippov <androsphilippos@gmail.com>
2023-09-14 00:27:16 +0500

    fix: fix Postcard and Modal js

--- src/js/modules/Modal/index.js
+++ src/js/modules/Modal/index.js
@@ -3,43 +3,43 @@ import { Postcard } from '../Postcard';
 import { ImageSrcMap } from './helpers';
 
 export const initModal = () => {
+  let target = null;
+
+  // init cards
   const cards = document.querySelector('.cards');
+
+  // init modal
   const modal = document.getElementById('modal');
   const modalWrapper = modal.querySelector('.modal__wrapper');
-  const draftCanvas = modal.querySelector('#draft');
+
+  // init form
   const form = modal.querySelector('.form');
   const buttonClose = form.querySelector('.form__close');
   const headingInput = form.querySelector('#form-input');
   const messageInput = form.querySelector('#form-textarea');
   const colorInput = form.querySelector('#form-color');
+  const formSubmit = form.querySelector('.form__submit');
+
+  // init canvases
+  const draftCanvas = modal.querySelector('#draft');
+  const finalCanvas = document.createElement('canvas');
+  const draft = new Postcard(draftCanvas);
+  const final = new Postcard(finalCanvas);
+
+  headingInput.addEventListener('input', (evt) => {
+    draft.wrapText(evt.target.value, TypeText.HEADING, true);
+  });
+  messageInput.addEventListener('input', (evt) => {
+    draft.wrapText(evt.target.value, TypeText.MESSAGE, true);
+  });
+  colorInput.addEventListener('change', (evt) => {
+    draft.setColor(evt.target.value);
+    draft.wrapText(headingInput.value, TypeText.HEADING, true);
+    draft.wrapText(messageInput.value, TypeText.MESSAGE, true);
+  });
 
   cards.addEventListener('click', (evt) => {
-    const target = evt.target.dataset.value;
-
-    let draft = new Postcard(draftCanvas);
-
-    const inputText = function (text, typeText, isLiveTyping) {
-      this.wrapText(text, typeText, isLiveTyping);
-    };
-    const inputColor = function (color, heading, message) {
-      this.setColor(color);
-      this.wrapText(heading, TypeText.HEADING, true);
-      this.wrapText(message, TypeText.MESSAGE, true);
-    };
-
-    const handleHeadingInput = (evt) => {
-      inputText.call(draft, evt.target.value, TypeText.HEADING, true);
-    };
-    const handleMessageInput = (evt) => {
-      inputText.call(draft, evt.target.value, TypeText.MESSAGE, true);
-    };
-    const handleColorInput = (evt) => {
-      inputColor.call(draft, evt.target.value, headingInput.value, messageInput.value);
-    };
-
-    headingInput.addEventListener('input', handleHeadingInput);
-    messageInput.addEventListener('input', handleMessageInput);
-    colorInput.addEventListener('change', handleColorInput);
+    target = evt.target.dataset.value;
 
     const img = document.createElement('img');
     img.setAttribute('src', ImageSrcMap[target]['1x']);
@@ -47,65 +47,57 @@ export const initModal = () => {
     img.setAttribute('alt', `image${target}-big`);
     img.setAttribute('width', 600);
     img.setAttribute('height', 900);
+    img.setAttribute('class', 'draft-image');
     modalWrapper.prepend(img);
 
     modal.showModal();
+  });
 
-    modal.addEventListener('close', () => {
-      headingInput.value = '';
-      messageInput.value = '';
-      colorInput.value = '#000000';
-      headingInput.removeEventListener('input', handleHeadingInput);
-      messageInput.removeEventListener('input', handleMessageInput);
-      colorInput.removeEventListener('change', handleColorInput);
-      img.remove();
-      draft.destroy();
-      draft = null;
-    });
+  modal.addEventListener('close', () => {
+    headingInput.value = '';
+    messageInput.value = '';
+    colorInput.value = '#000000';
+    draft.setColor('#000000');
+    draft.clear();
+    modal.querySelector('.draft-image').remove();
+  });
 
-    buttonClose.addEventListener('click', () => {
-      modal.close();
-    });
+  buttonClose.addEventListener('click', () => {
+    modal.close();
+  });
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
 
     const imgForJpeg = document.createElement('img');
     imgForJpeg.setAttribute('src', ImageSrcMap[target]['1x']);
 
-    form.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-
-      const formData = new FormData(form);
-      const heading = formData.get('heading');
-      const message = formData.get('message');
-      const color = formData.get('color');
-
-      const finalCanvas = document.createElement('canvas');
-      let final = new Postcard(finalCanvas);
+    imgForJpeg.onload = () => {
       final.drawImage(imgForJpeg);
-      final.setColor(color);
-      final.wrapText(heading, TypeText.HEADING, false);
-      final.wrapText(message, TypeText.MESSAGE, false);
+      final.setColor(colorInput.value);
+      final.wrapText(headingInput.value, TypeText.HEADING, false);
+      final.wrapText(messageInput.value, TypeText.MESSAGE, false);
       final.download();
-      final.destroy();
-      final = null;
+      final.clear();
+      imgForJpeg.remove();
+    };
 
-      const formSubmit = form.querySelector('.form__submit');
-      headingInput.setAttribute('disabled', true);
-      messageInput.setAttribute('disabled', true);
-      colorInput.setAttribute('disabled', true);
+    headingInput.setAttribute('disabled', true);
+    messageInput.setAttribute('disabled', true);
+    colorInput.setAttribute('disabled', true);
 
-      formSubmit.classList.add('success');
-      formSubmit.textContent = ButtonSubmitStatusText.SUCCESS;
-      formSubmit.setAttribute('disabled', true);
+    formSubmit.classList.add('success');
+    formSubmit.textContent = ButtonSubmitStatusText.SUCCESS;
+    formSubmit.setAttribute('disabled', true);
 
-      setTimeout(() => {
-        formSubmit.removeAttribute('disabled');
-        formSubmit.textContent = ButtonSubmitStatusText.IDLE;
-        formSubmit.classList.remove('success');
+    setTimeout(() => {
+      formSubmit.removeAttribute('disabled');
+      formSubmit.textContent = ButtonSubmitStatusText.IDLE;
+      formSubmit.classList.remove('success');
 
-        headingInput.removeAttribute('disabled');
-        messageInput.removeAttribute('disabled');
-        colorInput.removeAttribute('disabled');
-      }, 2000);
-    });
+      headingInput.removeAttribute('disabled');
+      messageInput.removeAttribute('disabled');
+      colorInput.removeAttribute('disabled');
+    }, 2000);
   });
 };
--- src/js/modules/Postcard/Postcard.js
+++ src/js/modules/Postcard/Postcard.js
@@ -31,9 +31,8 @@ export class Postcard {
     createEl.click();
   }
 
-  destroy() {
+  clear() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx = null;
   }
 
   setColor(color) {
